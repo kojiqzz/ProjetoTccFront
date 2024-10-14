bbe83b9bb4fb437fd821681e4a81934a3e7a643f
VitorFerreiraLandim <ra54573958800@acaonsfatima.org.br>
2024-10-13 23:14:38 -0300

    Api de login e cadastro quase terminada

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -1,15 +1,107 @@
 import './index.scss';
 import img2 from '../../assets/images/img2.png';
 import perfil from '../../assets/images/perfil.png';
-import telefone from '../../assets/images/telefone.png';
-import email from '../../assets/images/email.webp';
+import telefone1 from '../../assets/images/telefone.png';
+import email1 from '../../assets/images/email.webp';
 import InputMask from 'react-input-mask';
 import olho from '../../assets/images/olho2.png';
 import olhofechado from '../../assets/images/olho.png';
+import exclamacao from '../../assets/images/exclamation.png';
+import check from '../../assets/images/check.png'
 import { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function RegisterPage() {
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [confirmSenha, setConfirmSenha] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [mensagemErro, setMensagemErro] = useState('');
+    const [mensagemErroSenha, setMensagemErroSenha] = useState('');
+    const [modalAberto, setModalAberto] = useState(false);
+    const [mensagemSucesso, setMensagemSucesso] = useState('');
+
+    async function verificarTelefone(numero) {
+        const apiKey = '46cfd2afb53f3f0e875e9c6cf7e463b4';
+        
+       
+        const numeroComCodigo = numero.startsWith('+') ? numero : `+55${numero.replace(/\D/g, '')}`;
+    
+        const url = `http://apilayer.net/api/validate?access_key=${apiKey}&number=${numeroComCodigo}`;
+    
+        try {
+            const response = await axios.get(url);
+            console.erro('Resposta da API:', response.data);
+            return response.data.valid; 
+        } catch (error) {
+            console.error('Erro ao verificar telefone:', error);
+            return false;
+        }
+    }
+    
+
+    async function verificarEmail(email) {
+        
+        const url = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=5fae6172b8d6f7789a678d33f224a42667e52aaa`;
+        
+        try {
+            const response = await axios.get(url);
+            return response.data.data.result === 'valid';
+        } catch (error) {
+            console.error('Erro ao verificar e-mail:', error);
+            return false;
+        }
+    }
+
+    async function cadastrar() {
+        if (senha !== confirmSenha) {
+            setMensagemErroSenha('As senhas não coincidem!');
+            return;
+        }
+
+        const telefoneValido = await verificarTelefone(telefone);
+        const emailValido = await verificarEmail(email);
+
+        if (!telefoneValido) {
+            setMensagemErro('Número de telefone inválido. Verifique e tente novamente.');
+            return;
+        }
+
+        if (!emailValido) {
+            setMensagemErro('E-mail não está registrado. Verifique e tente novamente.');
+            return;
+        }
+
+        setMensagemErro('');
+
+        let userData = {
+            "nome": nome,
+            "telefone": telefone,
+            "email": email,
+            "senha": senha,
+        };
+
+        const url = `http://localhost:5001/cadastro`;
+        let resp = await axios.post(url, userData);
+        setMensagemSucesso('Cadastro concluído. Id: ' + resp.data.novoId);
+        setModalAberto(true); 
+    }
+
+    const limparMensagemErro = () => {
+        setMensagemErro('');
+    };
+
+    const fecharModal = () => {
+        setModalAberto(false);
+    };
+
+    const abrirModal = () => {
+        setModalAberto(true);
+    };
+
     return (
         <div className='div3'>
             <div className='login'>
@@ -18,31 +110,62 @@ export default function RegisterPage() {
                     <div className='inp'>
                         <div className='in1'>
                             <img src={perfil} alt="" />
-                            <input type="text" placeholder='Nome' />
+                            <input 
+                                type="text" 
+                                placeholder='Nome de usuario'
+                                value={nome}
+                                onChange={e => setNome(e.target.value)}
+                                onFocus={limparMensagemErro}
+                            />
                         </div>
                         <div className='in2'>
-                            <img src={telefone} alt="" />
-                            <InputMask mask="(99) 99999-9999" placeholder='Telefone' >
+                            <img src={telefone1} alt="" />
+                            <InputMask 
+                                mask="(99) 99999-9999" 
+                                placeholder='Telefone' 
+                                value={telefone}
+                                onChange={e => setTelefone(e.target.value)}
+                                onFocus={limparMensagemErro}
+                            >
                                 {(inputProps) => <input {...inputProps} />} 
                             </InputMask>
                         </div>
                         <div className='in3'>
-                            <img src={email} alt="" />
-                            <input type="text" placeholder='E-mail' />
+                            <img src={email1} alt="" />
+                            <input 
+                                type="text"
+                                placeholder='E-mail'
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                onFocus={limparMensagemErro}
+                            />
                         </div>
+                        
                         <div className='in4'>
-                            <img  src={img2} alt="" />
-                            <input type={showPassword ? 'text' : 'password'} placeholder='senha' />
+                            <img src={img2} alt="" />
+                            <input 
+                                type={showPassword ? 'text' : 'password'} 
+                                placeholder='senha' 
+                                value={senha}
+                                onChange={e => setSenha(e.target.value)}
+                                onFocus={limparMensagemErro}
+                            />
                             <img 
                                 className='password' 
                                 src={showPassword ? olho : olhofechado} 
                                 alt="" 
                                 onClick={() => setShowPassword(!showPassword)} 
                             />
-                            </div>
+                        </div>
                         <div className='in4'>
-                        <img  src={img2} alt="" />
-                            <input type={showPassword ? 'text' : 'password'} placeholder='confirmar senha' />
+                            <img src={img2} alt="" />
+                            <input 
+                                type={showPassword ? 'text' : 'password'}
+                                placeholder='confirmar senha'
+                                value={confirmSenha}
+                                onChange={e => setConfirmSenha(e.target.value)}
+                                onFocus={limparMensagemErro}
+                            />
                             <img 
                                 className='password' 
                                 src={showPassword ? olho : olhofechado} 
@@ -50,9 +173,12 @@ export default function RegisterPage() {
                                 onClick={() => setShowPassword(!showPassword)} 
                             />
                         </div>
+                        {mensagemErro && <div className='error-message'><img src={exclamacao} alt="" />{mensagemErro}</div>}
+                        {mensagemErroSenha && <div className='error-message'><img src={exclamacao} alt="" />{mensagemErroSenha}</div>}
                     </div>    
                     <div className='botao'> 
-                        <button className='b1'>cadastrar</button>
+                        <button className='b1' onClick={cadastrar}>cadastrar</button>
+                        <Link to='/' className='b2'>voltar</Link> 
                     </div>
                 </div>
                 <div className='bem-vinda'>
@@ -60,6 +186,17 @@ export default function RegisterPage() {
                     <h1 className='h2'>Pronta para realçar sua beleza?</h1>
                 </div>
             </div>
+
+            {modalAberto && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <img src={check} alt="" />
+                        <h2>PARABÉNS!</h2>
+                        <p>Cadastro realizado com sucesso!</p>
+                        <button onClick={fecharModal}>fechar</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,11 +3,36 @@ import img2 from '../../assets/images/img2.png';
 import olho from '../../assets/images/olho2.png';
 import olhofechado from '../../assets/images/olho.png';
 import { Link } from 'react-router-dom';
-import email from '../../assets/images/email.webp';
+import perfil from '../../assets/images/email.webp';
 import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
     const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+
+    const navigate = useNavigate()
+
+    async function entrar() {
+        const usuario = {
+            "email": email,
+            "senha": senha
+        }
+
+        const url = `http://localhost:5001/entrar/`
+        let resp = await axios.post(url, usuario)
+
+        
+        if (resp.data.erro != undefined) {
+            alert(resp.data.erro)
+        } else {
+            localStorage.setItem('USUARIO', resp.data.token)
+            navigate('/inicio')
+        }
+    }
 
     return (
         <div className='div2'>
@@ -16,12 +41,21 @@ export default function LoginPage() {
                     <h1>Login</h1>
                     <div className='inp'>
                         <div className='in1'>
-                            <img src={email} alt="" />
-                            <input type="text" placeholder='E-mail' />
+                            <img className='img' src={perfil} alt="" />
+                            <input 
+                            type="text" 
+                            placeholder='E-mail'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className='in2'>
                             <img className='img1' src={img2} alt="" />
-                            <input type={showPassword ? 'text' : 'password'} placeholder='senha' />
+                            <input 
+                            type={showPassword ? 'text' : 'password'} 
+                            placeholder='senha' 
+                            value={senha}
+                            onChange={(e) => setSenha(e.target.value)}/>
                             <img 
                                 className='password' 
                                 src={showPassword ? olho : olhofechado} 
@@ -33,7 +67,7 @@ export default function LoginPage() {
                     <Link className='senha' to='/redefinicaoSenha'>esqueceu sua senha?</Link>   
                     <div className='botao'> 
                         <Link to='/cadastro' className='b1'>cadastrar-se</Link>
-                        <Link to='/inicio' className='b2'>entrar</Link>
+                        <Link  className='b2' onClick={entrar} >entrar</Link>
                     </div>
                 </div>
                 <div className='bem-vinda'>
